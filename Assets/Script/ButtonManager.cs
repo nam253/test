22392ca51bb67750aec43e5c225d0372502e86b0
nam253 <namye6950@gmail.com>
2025-06-07 17:36:09 +0900

    new

--- Assets/Script/ButtonManager.cs
+++ Assets/Script/ButtonManager.cs
@@ -13,7 +13,7 @@ public class ButtonManager : MonoBehaviour
             Combo.bestscore = Combo.score;
         }
 
-        bestScoreText.text = "Best : " + Combo.bestscore.ToString();
+        bestScoreText.text = "BestScore : " + Combo.bestscore.ToString();
     }
 
     public void gamerestart()
